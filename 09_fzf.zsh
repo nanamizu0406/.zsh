94b31024d3c86dfefa9c6a5b5ac4810c4e86f26c
kato <kaito0406goak@gmail.com>
2019-01-05 18:38:08 +0900

    fzf.zshの追加

--- 09_fzf.zsh
+++ 09_fzf.zsh
@@ -1,3 +1,45 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git"'
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
+
+#ディレクトリ配下のディレクトリに移動する
+cd-fzf-find() {
+    local DIR=$(find ./ -path '*/\.*' -name .git -prune -o -type d -print 2> /dev/null | fzf +m)
+    if [ -n "$DIR" ]; then
+        cd $DIR
+    fi
+}
+alias fd=cd-fzf-find
+
+#ディレクトリ配下のファイルをemacsで開く
+emacs-fzf-find() {
+    local FILE=$(find ./ -path '*/\.*' -name .git -prune -o -type f -print 2> /dev/null | fzf +m)
+    if [ -n "$FILE" ]; then
+        ${EDITOR:-cemacs} $FILE
+    fi
+}
+alias fe=emacs-fzf-find
+
+#コマンド履歴
+function buffer-fzf-history() {
+    local HISTORY=$(history -n -r 1 | fzf +m)
+    BUFFER=$HISTORY
+    if [ -n "$HISTORY" ]; then
+        CURSOR=$#BUFFER
+    else
+        zle accept-line
+    fi
+}
+zle -N buffer-fzf-history
+bindkey '^R' buffer-fzf-history
+
+#gitのブランチを切り替える
+function checkout-fzf-gitbranch() {
+    local GIT_BRANCH=$(git branch --all | grep -v HEAD | fzf +m)
+    if [ -n "$GIT_BRANCH" ]; then
+        git checkout $(echo "$GIT_BRANCH" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    fi
+    zle accept-line
+}
+zle -N checkout-fzf-gitbranch
+bindkey '^O' checkout-fzf-gitbranch
